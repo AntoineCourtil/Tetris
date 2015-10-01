c566f7e29a66e20f6fa54a96b2f66a2aab38383e
ssloy <spam@skisa.org>
2015-10-01 10:45:59 +0200

    GUI stub

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <Tableau.h>
+#include "game.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main(int argc, char* args[]) {
+    Game game;
+    game.init_sdl("Tetris", 480, 300, 32);
+    while (game.running()) {
+        game.handle_events();
+        game.draw();
+    }
+    game.clean();
     return 0;
-    Tableau();
 }
--- src/Tableau.cpp
+++ src/Tableau.cpp
@@ -2,15 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Tableau::Tableau()
-{
+Tableau::Tableau() {
     int i;
     int j;
 
-    for(i=0;i<TABLEAU_LARGEUR;i++)
-    {
-        for(j=0;j<TABLEAU_HAUTEUR;j++)
-        {
+    for(i=0; i<TABLEAU_LARGEUR; i++) {
+        for(j=0; j<TABLEAU_HAUTEUR; j++) {
             zone[i][j] = LIBRE;
             printf("#");
         }
@@ -18,6 +15,40 @@ Tableau::Tableau()
     }
 }
 
+void Tableau::render(SDL_Surface *screen) {
+    int scr_w = screen->w;
+    int scr_h = screen->h;
+    int sq_w  = scr_w/TABLEAU_LARGEUR;
+    int sq_h  = scr_h/TABLEAU_HAUTEUR;
+
+    for (int i=0; i<TABLEAU_LARGEUR; i++) {
+        for (int j=0; j<TABLEAU_HAUTEUR; j++) {
+            SDL_Rect rect;
+            rect.w = sq_w-1;
+            rect.h = sq_h-1;
+            rect.x = i*sq_w;
+            rect.y = j*sq_h;
+            SDL_FillRect(screen, &rect, SDL_MapRGB(screen->format, 255,127,80));
+        }
+    }
+
+
+/*
+    for (int i=0; i<(int)pieces_.size(); i++) {
+        for (int j=0; j<(int)pieces_[i].size(); j++) {
+            Vec2i pt = pieces_[i].pos + pieces_[i].cell(j)*square_size;
+            SDL_Rect tmp;
+            tmp.w = tmp.h = square_size-1;
+            tmp.x = pt.x;
+            tmp.y = pt.y;
+            SDL_FillRect(screen, &tmp, SDL_MapRGB(screen->format, pieces_[i].rgb[0], pieces_[i].rgb[1], pieces_[i].rgb[2]));
+        }
+    }
+*/
+}
+
+
+/*
 void Tableau::setpieceActuelle()
 {
     pieceActuelle = p;
@@ -53,3 +84,5 @@ void Board::floodFill(int i, int j, int px, int py, int k, int o, int valeur)
 
     flood(i, j, px, py, k, o, valeur, visite);
 }
+
+*/
